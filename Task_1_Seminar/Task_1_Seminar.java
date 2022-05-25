7204b3df58483bbe6980046f1f19dc16c76de1c9
arumarov <arumarov@yandex.ru>
2022-05-25 21:00:59 +0600

    create task1

---
+++ Task_1_Seminar/Task_1_Seminar.java
@@ -0,0 +1,16 @@
+package Task_1_Seminar;
+/**
+ * Task_1_Seminar
+ * Реализовать функцию возведения числа а в степень b. a, b ∈ Z. 
+ * Сводя количество выполняемых действий к минимуму. 
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+public class Task_1_Seminar {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
