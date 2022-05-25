c16eb6d59204cc0fcebc94cb445d7f6e0207319b
arumarov <arumarov@yandex.ru>
2022-05-25 23:20:30 +0600

    task 1. my

--- Task_1_Seminar/Task_1_Seminar.java
+++ Task_1_Seminar/Task_1_Seminar.java
@@ -1,16 +1,53 @@
 package Task_1_Seminar;
+
+
+
 /**
  * Task_1_Seminar
- * Реализовать функцию возведения числа а в степень b. a, b ∈ Z. 
- * Сводя количество выполняемых действий к минимуму. 
+ * Реализовать функцию возведения числа а в степень b. a, b ∈ Z.
+ * Сводя количество выполняемых действий к минимуму.
+ * входные данные находятся в файле input.txt в виде:
+ * b 3
+ * a 10
+ * Результат нужно сохранить в файле output.txt:
+ * 1000
  */
 
-import java.io.FileWriter;
-import java.io.IOException;
+// Мое решение (без записи в файл)
+// public class Task_1_Seminar {
+
+//     static double degree(int a, int b) {
+//         double x = Math.pow(a, b);
+//         return x;
+//     }
+//     public static void main(String[] args) {
+//         System.out.println(degree(2, 3));
+//     }
+// }
+
+// Решение с семинара
 public class Task_1_Seminar {
 
     public static void main(String[] args) {
+        System.out.println(calculatePow(2, 2));
+    }
 
-        
+    public static double calculatePow(double a, double b) {
+        double result = 1;
+        if (a==1 || b == 0) {
+            result = 1;
+        }
+        if (a==0) {
+            result = 0;
+        }
+        if (b < 0 || a != 0) {
+            a = 1/a;
+            b = -b;
+        }
+
+        for(int i = 0; i < b; i++){
+            result *= a;
+        }
+        return result;
     }
 }
