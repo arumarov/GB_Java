1d2a25e2edbdbd975c83b4c1a52085510e0ee38d
arumarov <arumarov@yandex.ru>
2022-05-28 22:46:19 +0600

    Homework1.Task2

---
+++ Seminar_1_Homework/Seminar_2_Homework.java
@@ -0,0 +1,20 @@
+public class Seminar_2_Homework {
+    public static void main(String[] args) {
+        System.out.println(subtractProductAndSum(234));
+        
+    }
+
+    public static int subtractProductAndSum(int n) {
+        int b = 0;
+        int sum = 0;
+        int prod = 1;
+        while (n>0){
+            b = n%10;
+            System.out.println(b);
+            sum = sum + b;
+            prod = prod * b;
+            n = n/10;
+        }
+        return prod-sum;
+    }
+}
