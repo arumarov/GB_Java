00ac375e7704505a396c35c4f35822cc4c4c84ae
arumarov <arumarov@yandex.ru>
2022-06-02 20:58:24 +0600

    Seminar_2. TAsk_1

--- program.java
+++ program.java
@@ -1,10 +1,8 @@
 public class program {
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int min = a < b ? a : b; // То есть если условие a<b выполняется, будет выполнена
-        // та часть, которая находится после ? и до :, если a<b не выполняется,
-        // то вернется значение между : и ;
-        System.out.println(min);
-    }
+  public static void main(String[] args) {
+      int arr[] = new int[]{1,2,3,4,5,77};
+      for (int item : arr) {
+          System.out.println(item);
+      }
+  }
 }
