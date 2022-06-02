9b5c0f38ab11b0260b7827ca3823fe776f59f092
arumarov <arumarov@yandex.ru>
2022-06-02 22:26:45 +0600

    Seminar_2. My solution task_3

--- Seminar_2/Task_3.java
+++ Seminar_2/Task_3.java
@@ -1,8 +1,30 @@
 package Seminar_2;
+/* Напишите метод, который принимает на вход строку (String) и 
+* определяет является ли строка палиндромом (возвращает boolean значение) */
 
-public class Task_3 {
-    public static void main(String[] args) {
+// Мое решение
+// public class Task_3 {
+//     public static void main(String[] args) {
+//        System.out.println(palindrom("abcba")); 
+//     }
+
+//     public static boolean palindrom(String a) {
+//         boolean result = false;
+//         for (int i = 0; i < a.length(); i++){
+//             char ch = a.charAt(i);
+//             if (ch != a.charAt(a.length() - 1 - i)) {
+//                 return result;
+//             }
+//             else{
+//                 result = true;
+//             }
+//         }
+
+//         return result;
         
-    }
+//     }
     
-}
+// }
+
+
+// Решение с семинара
