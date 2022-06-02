5792efff882c5d4f1705e0ea9c8185b84487413e
arumarov <arumarov@yandex.ru>
2022-06-02 22:01:52 +0600

    Seminar_2. End task_2

--- Seminar_2/Task_2.java
+++ Seminar_2/Task_2.java
@@ -1,25 +1,33 @@
+package Seminar_2;
 /*Попробовать сгенерировать все перестановки длины N из K чисел с повторениями. 
 * Пример: N = 2, K = 3 ответ "00 01 02 10 11 12 20 21 22"*/
 // Решение с помощью массива
 import java.util.Arrays;
 
-package Seminar_2;
+
 
 public class Task_2 {
     public static void main(String[] args) {
-        System.out.println(generate(2, 3));
+        int N = 3;
+        int K = 2; // сколько чисел будем использовать
+
+        int[]nums = new int[N]; //создается массив [0, 0, 0]
+        generated(nums, 0, K);
         
     }
 
-    public static String generate(int n, int k){
-        String result = "";
-        for (int i = 0; i <= n; i++){
-            for (int j = 0; j < k; j++){
-                result = result + i + j + " ";
-            }
+    public static void generated(int[] array, int index, int K){
+        if (index == array.length){ // если индекс не равен длине массива, опускаемся в цикл
+            System.out.println(Arrays.toString(array)); // метод, позволяющий распечатать массив
+            return;
         }
-        return result;
 
-    }
+        for (int i = 0; i <= K; i++){
+            array[index] = i;
+            generated(array, index+1, K);
+
+        }
     
+    }
+
 }
