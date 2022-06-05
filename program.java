976bcbcea09e4a1a2371b951d540f0777abe9c3e
arumarov <arumarov@yandex.ru>
2022-06-05 18:30:30 +0600

    Seminar_2_Homework_leetcode_1

--- program.java
+++ program.java
@@ -1,8 +1,18 @@
 public class program {
   public static void main(String[] args) {
-      int arr[] = new int[]{1,2,3,4,5,77};
-      for (int item : arr) {
-          System.out.println(item);
-      }
+    System.out.println(mergeAlternately("ab", "pqrs"));
+  }
+
+  public static String mergeAlternately(String word1, String word2) {
+    String merged = "";
+    int min = word1.length() < word2.length() ? word1.length() : word2.length();
+    String maxString = word1.length() < word2.length() ? word2 : word1;
+    for (int i = 0; i < min; i++) {
+      char ch1 = word1.charAt(i);
+      char ch2 = word2.charAt(i);
+      merged = merged + ch1 + ch2;
+    }
+    return merged + maxString.substring(min, maxString.length());
+
   }
 }
